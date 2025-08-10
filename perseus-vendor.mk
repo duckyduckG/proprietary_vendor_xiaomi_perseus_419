ea7f4dae941879c28d1a9e9f5ec6101985c6c89c
duckyduckG <duckyduckg65@gmail.com>
2025-08-10 14:10:27 +0500

    perseus: Regen vendor
    34c556c perseus: remove bluetooth blobs
    5efc774 perseus: disable dependency check for some blobs
    
    Signed-off-by: duckyduckG <duckyduckg65@gmail.com>

--- perseus-vendor.mk
+++ perseus-vendor.mk
@@ -44,7 +44,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/perseus/proprietary/vendor/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
     vendor/xiaomi/perseus/proprietary/vendor/etc/gpsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpsconfig.xml \
     vendor/xiaomi/perseus/proprietary/vendor/etc/hdr_config.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/hdr_config.cfg \
-    vendor/xiaomi/perseus/proprietary/vendor/etc/init/android.hardware.bluetooth@1.0-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.bluetooth@1.0-service-qti.rc \
     vendor/xiaomi/perseus/proprietary/vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.keymaster@4.0-service-qti.rc \
     vendor/xiaomi/perseus/proprietary/vendor/etc/init/vendor.qti.secure_element@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.secure_element@1.0-service.rc \
     vendor/xiaomi/perseus/proprietary/vendor/etc/lhd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lhd.conf \
@@ -230,15 +229,11 @@ PRODUCT_PACKAGES += \
     libtonemapalgo \
     libvidhance \
     android.hardware.gnss@2.0-impl-xiaomi \
-    android.hardware.bluetooth@1.0-impl-qti \
     android.hardware.secure_element@1.0-impl \
     fingerprint.fpc.sdm845 \
     fingerprint.goodix.sdm845 \
     flp.brcm \
     gps.brcm \
-    vendor.qti.hardware.bluetooth_sar@1.0-impl \
-    vendor.qti.hardware.btconfigstore@1.0-impl \
-    libbtnv \
     libdiagjni \
     libgf_ca \
     libgf_hal \
@@ -250,13 +245,11 @@ PRODUCT_PACKAGES += \
     sensors.halls \
     sensors.ssc \
     sensors.touch \
-    vendor.qti.hardware.bluetooth_sar@1.0 \
     dirac_resource \
     libarcsoft_dualcam_refocus_skel \
     misound_res \
     glgps \
     gps.cer \
-    android.hardware.bluetooth@1.0-service-qti \
     android.hardware.keymaster@4.0-service-qti \
     vendor.qti.secure_element@1.0-service \
     ignss_2_0 \
